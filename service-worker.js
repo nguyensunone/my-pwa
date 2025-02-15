730dd2b3b73ce5ae5a408bcbbe362b0a8ee66175
nguyensunone <nguyensunone@gmail.com>
2025-02-14 19:40:45 -0800

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,23 +1,14 @@
-self.addEventListener('install', function(event) {
-    event.waitUntil(
-        caches.open('my-pwa-cache').then(function(cache) {
-            return cache.addAll([
-                '/',
-                '/index.html',
-                '/audio_test_offline.html',
-                '/audio_files/sample-audio.mp3',  // Thêm tất cả các tệp âm thanh cần thiết
-                '/audio_files/another-audio.mp3',
-                '/audio-mapping.json',  // Thêm tệp audio-mapping.json                
-                // Thêm các tệp khác nếu cần thiết
-            ]);
-        })
-    );
-});
-
-self.addEventListener('fetch', function(event) {
-    event.respondWith(
-        caches.match(event.request).then(function(response) {
-            return response || fetch(event.request);
-        })
-    );
+self.addEventListener('install', function(event) {  
+    event.waitUntil(  
+        caches.open('my-pwa-cache').then(function(cache) {  
+            return cache.addAll([  
+                '/',  
+                '/index.html',  
+                '/audio_test_offline.html',  
+                // Gỡ bỏ sample-audio.mp3 nếu không cần  
+                '/audio_files/another-audio.mp3',  
+                '/audio-mapping.json',  
+            ]);  
+        })  
+    );  
 });
