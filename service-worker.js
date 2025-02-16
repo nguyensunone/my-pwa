d7108878fde0cffe852e9a509e2baad0a2d6f971
nguyensunone <nguyensunone@gmail.com>
2025-02-15 19:14:45 -0800

    Restore main files

--- service-worker.js
+++ service-worker.js
@@ -1,45 +1,14 @@
-// Tên cache
-const CACHE_NAME = 'my-pwa-cache-v1';
-
-// Danh sách file cần cache
-const urlsToCache = [
-    '/nguyensunone.github.io/index.html',  
-    '/nguyensunone.github.io/manifest.json',
-    '/nguyensunone.github.io/icon-192x192.png',
-    '/nguyensunone.github.io/audio_test_offline.html',
-    '/nguyensunone.github.io/audio_files/another-audio.mp3',
-    '/nguyensunone.github.io/audio-mapping.json'
-];
-
-// Sự kiện cài đặt Service Worker
-self.addEventListener('install', function(event) {
-    event.waitUntil(
-        caches.open(CACHE_NAME)
-            .then(function(cache) {
-                console.log('Opened cache');
-                return cache.addAll(urlsToCache);
-            })
-    );
-});
-
-// Sự kiện fetch: Kiểm tra cache trước khi tải từ mạng
-self.addEventListener('fetch', function(event) {
-    event.respondWith(
-        caches.match(event.request)
-            .then(function(response) {
-                return response || fetch(event.request);
-            })
-    );
-});
-
-// Sự kiện kích hoạt (Xóa cache cũ nếu cần)
-self.addEventListener('activate', function(event) {
-    event.waitUntil(
-        caches.keys().then(function(cacheNames) {
-            return Promise.all(
-                cacheNames.filter(name => name !== CACHE_NAME)
-                    .map(name => caches.delete(name))
-            );
-        })
-    );
+self.addEventListener('install', function(event) {  
+    event.waitUntil(  
+        caches.open('my-pwa-cache').then(function(cache) {  
+            return cache.addAll([  
+                '/',  
+                '/index.html',  
+                '/audio_test_offline.html',  
+                // Gỡ bỏ sample-audio.mp3 nếu không cần  
+                '/audio_files/another-audio.mp3',  
+                '/audio-mapping.json',  
+            ]);  
+        })  
+    );  
 });
